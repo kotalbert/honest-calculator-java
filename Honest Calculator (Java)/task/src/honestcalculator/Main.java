b9f07e38c986f9d3ecd6181b66604b6783ccebd9
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-12-11 22:17:44 +0100

    Implement stage 3

--- Honest Calculator (Java)/task/src/honestcalculator/Main.java
+++ Honest Calculator (Java)/task/src/honestcalculator/Main.java
@@ -10,8 +10,11 @@ public class Main {
         String MSG_1 = "Do you even know what numbers are? Stay focused!";
         String MSG_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?";
         String MSG_3 = "Yeah... division by zero. Smart move...";
+        String MSG_4 = "Do you want to store the result? (y / n):";
+        String MSG_5 = "Do you want to continue calculations? (y / n):";
 
         Scanner sc = new Scanner(System.in);
+        float memory = 0;
         while (true) {
             System.out.println(MSG_0);
             String calc = sc.nextLine();
@@ -20,6 +23,14 @@ public class Main {
             x = split[0];
             oper = split[1];
             y = split[2];
+
+            if (x.equals("M")) {
+                x = Float.toString(memory);
+            }
+            if (y.equals("M")) {
+                y = Float.toString(memory);
+            }
+
             try {
                 Float.parseFloat(x);
                 Float.parseFloat(y);
@@ -31,6 +42,7 @@ public class Main {
                 System.out.println(MSG_2);
                 continue;
             }
+
             float xf = Float.parseFloat(x);
             float yf = Float.parseFloat(y);
 
@@ -46,7 +58,17 @@ public class Main {
                 default -> 0; // This should never happen due to the regex check above
             };
             System.out.println(result);
-            return;
+            System.out.println(MSG_4);
+            String store = sc.nextLine();
+            if (store.equals("y")) {
+                memory = result;
+            }
+            System.out.println(MSG_5);
+            String cont = sc.nextLine();
+            if (cont.equals("n")) {
+                break;
+            }
+
         }
 
 
